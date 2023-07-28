d4c7f71e06b99e56ddcb84ba8f2eb7ad193f8ea8
xLalice <dreamsend08@gmail.com>
2023-07-28 21:20:45 +0800

    Create basic HTML elements

---
+++ Random Quotes/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import './App.css'
+
+export default function App(){
+  return (
+    <div id="quote-box">
+      <h1 id="text"></h1>
+      <h2 id="author"></h2>
+      <button>New Quote</button>
+      <button>Tweet Quote</button>
+    </div>
+  )
+}
