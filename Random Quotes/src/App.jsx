57ad854d22a46fe91109170de2bac458831acb2b
xLalice <dreamsend08@gmail.com>
2023-07-29 00:31:53 +0800

    Add category functionality

--- Random Quotes/src/App.jsx
+++ Random Quotes/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import options from "./assets/categories.js"
+
 
 export default function App() {
   const apiKey = "TyOwcUenvZiMWoapLn5eNw==kaHFXaIspzzCAafB"
   const [quotes, setQuotes] = React.useState([]);
+  const [category, setCategory] = React.useState("")
   const [isLoading, setIsLoading] = React.useState(true);
+
+  
+  
 
   React.useEffect(() => {
     fetchQuotes();
@@ -12,7 +18,7 @@ export default function App() {
 
   const fetchQuotes = async () => {
     try {
-      const response = await fetch(`https://api.api-ninjas.com/v1/quotes`, {
+      const response = await fetch(`https://api.api-ninjas.com/v1/quotes${category ? `?category=${category}` : ""}`, {
         headers: {
           'X-Api-Key': apiKey,
         },
@@ -25,19 +31,51 @@ export default function App() {
     }
   };
 
+  function tweetQuote() {
+    const tweetText = encodeURIComponent(quotes[0].quote + " - " + quotes[0].author);
+    window.open(`https://twitter.com/intent/tweet?text=${tweetText}`, '_blank');
+  };
+
+  const categories = options.map(item => (
+    <option value={item.value}>{item.label}</option>
+  ))
+
+  function handleChange(event){
+    setCategory(event.target.value)
+    console.log(category)
+  }
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
+  
   return (
     <div id="quote-box">
-      <button onClick={fetchQuotes} id="new-quote">New Quote</button>
+      
       {quotes.length > 0 && (
         <div>
           <p id="text">{quotes[0].quote}</p>
           <p id="author">- {quotes[0].author || 'Unknown'}</p>
         </div>
       )}
+      <div id="controls">
+        <button className="btn btn-primary" onClick={fetchQuotes} id="new-quote">New Quote</button>
+        <button className="btn btn-primary"><a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(quotes[0].quote + " - " + quotes[0].author)}`} target="_blank" onClick={tweetQuote}>
+            Tweet Quote 
+        </a></button>
+        <select 
+          id="category" 
+          className="form-select form-select-lg mb-3"
+          value={category}
+          onChange={handleChange}
+          name={category}
+        >
+          <option disabled value="">Category</option>
+          {categories}
+        </select>
+      </div>
+      
     </div>
   );
 };
