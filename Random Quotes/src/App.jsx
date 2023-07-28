63fc20435a691be4a6833378e9788956f3fd7eda
xLalice <dreamsend08@gmail.com>
2023-07-28 22:48:58 +0800

    Get quote from Api Ninja

--- Random Quotes/src/App.jsx
+++ Random Quotes/src/App.jsx
@@ -1,13 +1,53 @@
-import React from "react"
-import './App.css'
+import React from 'react';
 
-export default function App(){
+export default function App() {
+  const apiKey = "TyOwcUenvZiMWoapLn5eNw==kaHFXaIspzzCAafB"
+  const [quotes, setQuotes] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetchQuotes();
+  }, []);
+
+  /* const fetchQuotes = async () => {
+    try {
+      const response = await fetch('https://api.api-ninjas.com/v1/quotes');
+      const data = await response.json();
+      setQuotes(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching quotes:', error);
+    }
+  }; */
+
+  const fetchQuotes = async () => {
+    try {
+      const response = await fetch(`https://api.api-ninjas.com/v1/quotes`, {
+        headers: {
+          'X-Api-Key': apiKey,
+        },
+      });
+      const data = await response.json();
+      setQuotes(data);
+      setIsLoading(false)
+    } catch (error) {
+      console.error('Error fetching quotes:', error);
+    }
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div id="quote-box">
-      <h1 id="text"></h1>
-      <h2 id="author"></h2>
-      <button>New Quote</button>
-      <button>Tweet Quote</button>
+      <button onClick={fetchQuotes} id="new-quote">New Quote</button>
+      {quotes.length > 0 && (
+        <div>
+          <p id="text">{quotes[0].quote}</p>
+          <p id="author">- {quotes[0].author || 'Unknown'}</p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
